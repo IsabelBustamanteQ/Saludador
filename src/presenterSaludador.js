859e2e5dae472c6019c898519676a8c3e46a305c
Isabel <isabqm121@gmail.com>
2023-02-16 12:45:15 -0400

    Saludo por genero y edad

--- src/presenterSaludador.js
+++ src/presenterSaludador.js
@@ -1,26 +1,54 @@
 //import sumar from "./sumador";
 
 const nombre = document.querySelector("#nombre");
-var genero = document.querySelector("#genero").value;
-//var selected=genero.options[genero.selectexIndex].text;
-//alert(genero);
+//var genero = document.querySelector("#genero").value;
+
+
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 var genero = document.querySelector("#genero").value;
+var edad=document.querySelector("#edad").value;
+if(edad ==="")
+{
 var x="";
-  if (genero === "1") {
-    x = "Sra.";
-  } else {
-    if (genero === "2") {
-        x = "Sr.";
-      } else {
-        if (genero === "3" ) {
-            x = "Sre.";
-          } 
+} 
+  if(edad>30 || edad==="")
+  {
+    if (genero === "1") 
+    {
+      x = "Sra. ";
+    }
+    else
+    {
+      if (genero === "2")
+      {
+        x = "Sr. ";
       }
+      else
+      {
+        if (genero === "3" )
+        {
+          x = "Sre. ";
+        } 
+      }
+    }
+  }
+  else
+  {
+    if (genero === "1") {
+      x = "Srta. ";
+    } else {
+      if (genero === "2") {
+          x = "Srto. ";
+        } else {
+          if (genero === "3" ) {
+              x = "Srte. ";
+            } 
+        }
+    }
   }
   div.innerHTML = "Hola "+ x + nombre.value;
 });

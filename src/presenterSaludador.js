b8267f0fdaf827d3f451495b3af47fcd8903e91d
Isabel <isabqm121@gmail.com>
2023-02-16 12:18:53 -0400

    Saludo por genero funcional

--- src/presenterSaludador.js
+++ src/presenterSaludador.js
@@ -1,15 +1,27 @@
 //import sumar from "./sumador";
 
 const nombre = document.querySelector("#nombre");
+var genero = document.querySelector("#genero").value;
+//var selected=genero.options[genero.selectexIndex].text;
+//alert(genero);
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  //const firstNumber = Number.parseInt(first.value);
-  //const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "Hola "+nombre.value;
+var genero = document.querySelector("#genero").value;
+var x="";
+  if (genero === "1") {
+    x = "Sra.";
+  } else {
+    if (genero === "2") {
+        x = "Sr.";
+      } else {
+        if (genero === "3" ) {
+            x = "Sre.";
+          } 
+      }
+  }
+  div.innerHTML = "Hola "+ x + nombre.value;
 });
 

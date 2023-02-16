0232e62d94e324fc528c56bd9bd3901b29c64b3e
Isabel <isabqm121@gmail.com>
2023-02-16 09:37:33 -0400

    Aumenta funcionalidad para saludar por nombre

--- src/presenterSaludador.js
+++ src/presenterSaludador.js
@@ -2,7 +2,7 @@
 
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludador-form");
-const div = document.querySelector("#saludo-div");
+const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();

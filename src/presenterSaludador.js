aff0d96a257a2c7d244dc7f915f9237c109b7d73
Isabel <isabqm121@gmail.com>
2023-02-17 19:08:40 -0400

    Saludos en Español e Ingles

--- src/presenterSaludador.js
+++ src/presenterSaludador.js
@@ -7,44 +7,39 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 var genero = document.querySelector("#genero").value;
+var idioma = document.querySelector("#idioma").value;
 var edad=document.querySelector("#edad").value;
-if(edad ==="")
+if(idioma=="0")
 {
-var x="";
-} 
+  if(edad==="")
+  {
+    titulo="";
+  }
   if(edad>30 || edad==="")
   {
     if (genero === "1") 
     {
-      x = "Sra. ";
+      titulo = "Sra. ";
     }
     else
     {
       if (genero === "2")
       {
-        x = "Sr. ";
-      }
-      else
-      {
-        if (genero === "3" )
-        {
-          x = "Sre. ";
-        } 
+        titulo = "Sr. ";
       }
     }
   }
   else
   {
-    if (genero === "1") {
-      x = "Srta. ";
-    } else {
-      if (genero === "2") {
-          x = "Srto. ";
-        } else {
-          if (genero === "3" ) {
-              x = "Srte. ";
-            } 
-        }
+    if (genero === "1") 
+    {
+      titulo = "Srta. ";
+    } else 
+    {
+      if (genero === "2")
+      {
+        titulo = "Srto. ";
+      } 
     }
   }
   var saludo="Hola";
@@ -63,6 +58,57 @@ var x="";
       saludo="Buenas Noches ";
     }
   }
-  div.innerHTML = saludo+ x + nombre.value;
+}
+else
+{
+  if(edad==="")
+  {
+    titulo="";
+  }
+  if(edad>30 || edad==="")
+  {
+    if (genero === "1") 
+    {
+      titulo = "Mrs. ";
+    }
+    else
+    {
+      if (genero === "2")
+      {
+        titulo = "Mr. ";
+      }
+    }
+  }
+  else
+  {
+    if (genero === "1") 
+    {
+      titulo = "Ms. ";
+    } else 
+    {
+      if (genero === "2")
+      {
+        titulo = "Mr. ";
+      } 
+    }
+  }
+  var saludo="Hello ";
+  if(fecha.getHours()<12)
+  {
+    saludo="Good morning ";
+  }
+  else
+  {
+    if(fecha.getHours()<19)
+    {
+      saludo="Good afternoon ";
+    }
+    else
+    {
+      saludo="Good evening ";
+    }
+  }
+}
+  div.innerHTML = saludo+ titulo + nombre.value;
 });
 
